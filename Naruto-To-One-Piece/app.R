7a17989554157c31995486ad9d3357f1e1c12489
Minjue Wu <wminjue@gmail.com>
2019-11-21 21:42:28 -0500

    ANIME data and 8/20 image upload

---
+++ Naruto-To-One-Piece/app.R
@@ -0,0 +1,139 @@
+library(shiny)
+library(shinyjs)
+library(readr)
+library(dplyr)
+library(tidyverse)
+
+narr <- read_rds("narr.rds")
+
+ui <- fluidPage(
+  useShinyjs(),
+  navbarPage(
+    "From Naruto to One Piece: Rise of Anime",
+    tabPanel(
+      "Ranking and Popularity Trends",
+      plotOutput("followers")
+    ),
+    tabPanel(
+      "Model Explanations"
+    ),
+    tabPanel(
+      "About",
+      htmlOutput("intro"),
+      htmlOutput("hitter"),
+      uiOutput("rsource"),
+      htmlOutput("more"),
+      htmlOutput("source"),
+      uiOutput("tab"),
+      uiOutput("tab2"),
+      htmlOutput("bio")
+    )
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+  output$intro <- renderUI({
+    HTML("<b><font size=6> Anime: Rise of a Global Phenomenon</font></b>
+                            <br><br>
+                            <p> First created in 1917 with propaganda films such as Momotarō: Umi no Shinpei, 
+                            Japanese animation (anime) swept the world in the 1980s as a global entertainment 
+                            phenonemon with the rise of mecha, superhero series, and space operas. As the focus 
+                            of anime shifted from film studio features such as Akira and Spirited Away, the format 
+                            of popular anime transformed into increasingly long series consisting of shorter 
+                            episodes — bite sized-chunks which can be pitched as more accessible, lower commitment, 
+                            and reduced-stakes trial runs to mold to ever-shifting popular opinion. As of 2014, 
+                            there are over 58 million hashtags for the topic of #anime, and in 2017 the the anime 
+                            sector earned over $19 billion USD worldwide. The burning question follows: with such a 
+                            high following and market shares, which anime series are responsible for bringing in 
+                            most of the revenue, and how do they manage to successfully (do or they?) captivate 
+                            audiences over decades of airing?<b><br><br>")})
+  
+  output$hitter <- renderUI({
+    HTML("<b><font size=4>Rationale: Heavy Hitter's Frenzy </font></b> 
+                            <br>
+                             A glance at the Top Anime list on MyAnimeList, a manga/anime social networking and 
+                             cataloging website with over 120 million visitors per month, reveals the most popular 
+                             hits are consistently dominated by specific, classic anime shows that have aired 
+                             hundreds of episodes over years. Fullmetal Alchemist:Brotherhood, for example, the 
+                             second  64-episode reboot of the original Fullmetal adaptaion, boasts over a million 
+                             members involved in forums, which doesn't even account for the amount of visiting 
+                             members who simply leave ratings. This project seeks to analyze trends across years 
+                             in ranking, popularity, audience size, and overall quality ratings of the 10 most 
+                             popular long-running (and still running!) anime shows in history. These include Naruto, 
+                             Bleach, Fullmetal Alchemist, Keroro Gunsō, Gin Tama, One Piece, Detective Conan (Case 
+                             Closed), Crayon Shin-Chan, Dragon Ball Z, and Sazae-san. Ranging from 120 to over 1600 
+                             episodes each, these series and their offshoots have evolved over the past decade, and 
+                             data gathered from various Anime review sites and new sources give a comprehensive 
+                             overview of how popular ratings has changed from 2006-2016.
+                             ")
+  })
+  
+  output$more <- renderUI({
+    HTML("<br><b> <font size=4>The More the Merrier...?</font> </b><br>
+                             Although the global size of adoring otaku(anime fanatic) populations and never-ceasing 
+                             news of the next Gin Tama season might seem like concrete assurance of these anime series' 
+                             longstanding welcome, capturing tantalizing original content is a precarious struggle. 
+                             Take these recent Naruto reviews from Kotaku Australia, and IGN, 
+                             for example. With well over 500 episodes in the original franchise alone, this 
+                             well-established ninja adventure series is now on its second generation — literally — 
+                             as the last 126 episodes follow Naruto's son Boruto, who now carries his father's mantle. 
+                             However, there is only so much one can do within the same worldbuilding parameters of a 
+                             small ninja village before episodes start to cycle old themes, fight sequences, and dialogue. 
+                             Die-hard fans who have suffered through the entire series specifically square off hundreds of 
+                             episodes as to-be-avoided &#8220filler&#8221 fluff to newcomers, and a sizable portion of episode 
+                             reviews feature negative commentary such as &#8220repetitive...poor AI&#8221, &#8220painfully 
+                             long&#8221, and &#8220overrated&#8221. 
+                            <br><br>")
+  })
+  
+  output$source <- renderUI({
+    HTML("<b> <font size=4>Data Sources</font> </b>
+                              <br>
+                              My data includes three datasets. One is a raw csv file processed by “rfordatascience” which compiles 
+                              the raw informational categories of anime series on MyAnimeList. This provides the basic background 
+                              info about almost every anime series I could hope to analyze, giving key filters such as producers, 
+                              genre, studio, number of episodes, airing date, duration, and date of premiere.
+                              My second and third databases are CSV files of anime recommendation ratings on Kaggle. The first of 
+                              these is scraped from user preference data from 73,516 anime networking site users on 12,294 anime titles, 
+                              including movie titles instead of only multi-episode series. This contains a broader range of users and 
+                              standardizes rantings on a 10 point scale across different anime review sites. My third database is 
+                              compiled from 2006-2015 anime reviews from Anime News Network, a major anime industry news website 
+                              which reaches audiences from Canada, Australia, U.S., and Southeast Asia.  <br> <br>")
+
+  })
+  
+  url0 <- a("Top Anime List", href = "https://myanimelist.net/topanime.php?type=bypopularity")
+
+  output$rsource <- renderUI({
+    tagList("Source: MyAnimeList:", url0)
+  })
+  
+
+  url1 <- a("Kotaku Australia", href = "https://www.kotaku.com.au/2014/11/naruto-is-fun-and-action-filled-but-its-also-repetitive-and-painfully-long/")
+  
+  output$tab <- renderUI({
+    tagList("URLs: Raw anime information database processed by", url1)
+  })
+  
+  url2 <- a("https://about.twitter.com/en_us/values/elections-integrity.html#data", href = " https://about.twitter.com/en_us/values/elections-integrity.html#data")
+  
+  output$tab2 <- renderUI({
+    tagList("Data source:", url2)
+  })
+  
+  
+  output$bio <- renderUI({
+    HTML("<br> <b> <font size=4>About Me:</font></b>
+                            My name is Minjue Wu and I am a sophomore at Harvard College studying History of
+                            Science and Music with a secondary in Global Health and Health Policy. I was born in China and raised on a healthy diet of 
+                             Asian animated shows, many of which are imported from Japan, and I am fascinated by the trends and evolution of these
+                            productions.
+         <br> <br>")
+  })
+  
+  output$followers <- renderPlot(narr)
+}
+
+shinyApp(ui, server)
---
+++ Naruto-To-One-Piece/prep.shiny.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(ggplot2)
+library(sf)
+library(fs)
+
+
+file_copy("images/narr.rds", "Naruto-To-One-Piece/narr.rds")
+file_copy("images/narp.rds", "Naruto-To-One-Piece/narp.rds")
+file_copy("images/gintamr.rds", "Naruto-To-One-Piece/gintamr.rds")
+file_copy("images/gintamp.rds", "Naruto-To-One-Piece/gintamp.rds")
+file_copy("images/fmar.rds", "Naruto-To-One-Piece/fmar.rds")
+file_copy("images/fmap.rds", "Naruto-To-One-Piece/fmap.rds")
+file_copy("images/blear.rds", "Naruto-To-One-Piece/blear.rds")
+file_copy("images/bleap.rds", "Naruto-To-One-Piece/bleap.rds")
